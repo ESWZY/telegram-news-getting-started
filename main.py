3d8f7317feb473fc1467c21f38ddc16dee0b9497
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2022-01-22 21:50:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -138,10 +138,10 @@ ie1.set_source_selector('span.sourceTemplate')
 ie1.max_post_length = 2000
 
 # News postman to manage sending affair
-np1 = NewsPostman(listURLs=[url4, ], sendList=[channel, ], db=db, tag=tag4)
+np1 = NewsPostman(listURLs=[url6, ], sendList=[channel, ], db=db, tag=tag6)
 np1.set_bot_token(bot_token)
 np1.set_extractor(ie1)
-np1.set_table_name(table_name4)
+np1.set_table_name(table_name6)
 np1.set_max_list_length(45)
 np1.set_max_table_rows(25 * 3, False)
 np1.poll()
