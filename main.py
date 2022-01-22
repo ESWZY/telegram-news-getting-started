2a10fdb71747224f3c31242efc39962c4378cca1
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2022-01-22 16:41:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ table_name3 = "admit"
 ie1 = InfoExtractor()
 
 # Select elements by CSS-based selector
-ie1.set_list_selector('.inner_page > ul > li') #id_ul_li
-ie1.set_title_selector('.inner_page > h4')  #id
-ie1.set_paragraph_selector('.inner_page > ul > li > a')
+ie1.set_list_selector('div.inner_page > ul > li') #id_ul_li
+ie1.set_title_selector('h4')  #id
+ie1.set_paragraph_selector('a')
 ie1.set_time_selector('')
 ie1.set_source_selector('span.sourceTemplate')
 ie1.max_post_length = 2000
