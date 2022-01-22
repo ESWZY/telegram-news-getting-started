7e5a40c18eee155799ade42b875036bd014a1825
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2022-01-22 15:47:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,32 +15,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 db = Session(bind=engine.connect())
 
-#-------------------------channel 1----------------------------------#
-
-url1 = "https://ssc.nic.in/Portal/LatestNews"
-tag1 = "ssc"
-table_name1 = "sscnews"
-
-# Info extractor to process data format
-ie1 = InfoExtractor()
-
-# Select elements by CSS-based selector
-ie1.set_list_selector('#forScrollNews > ul > li') #id_ul_li
-ie1.set_title_selector('h3')  #id
-ie1.set_paragraph_selector('a')
-ie1.set_time_selector('span')
-ie1.set_source_selector('span.sourceTemplate')
-ie1.max_post_length = 2000
-
-# News postman to manage sending affair
-np1 = NewsPostman(listURLs=[url1, ], sendList=[channel, ], db=db, tag=tag1)
-np1.set_bot_token(bot_token)
-np1.set_extractor(ie1)
-np1.set_table_name(table_name1)
-np1.set_max_list_length(25)
-np1.set_max_table_rows(25 * 3, False)
-np1.poll()
-
 #-------------------------channel 3----------------------------------#
 
 url3 = "https://sscnr.nic.in/newlook/site/admit_card.html"
@@ -63,6 +37,31 @@ np1 = NewsPostman(listURLs=[url3, ], sendList=[channel, ], db=db, tag=tag3)
 np1.set_bot_token(bot_token)
 np1.set_extractor(ie1)
 np1.set_table_name(table_name3)
+np1.set_max_list_length(25)
+np1.set_max_table_rows(25 * 3, False)
+np1.poll()
+#-------------------------channel 1----------------------------------#
+
+url1 = "https://ssc.nic.in/Portal/LatestNews"
+tag1 = "ssc"
+table_name1 = "sscnews"
+
+# Info extractor to process data format
+ie1 = InfoExtractor()
+
+# Select elements by CSS-based selector
+ie1.set_list_selector('#forScrollNews > ul > li') #id_ul_li
+ie1.set_title_selector('h3')  #id
+ie1.set_paragraph_selector('a')
+ie1.set_time_selector('span')
+ie1.set_source_selector('span.sourceTemplate')
+ie1.max_post_length = 2000
+
+# News postman to manage sending affair
+np1 = NewsPostman(listURLs=[url1, ], sendList=[channel, ], db=db, tag=tag1)
+np1.set_bot_token(bot_token)
+np1.set_extractor(ie1)
+np1.set_table_name(table_name1)
 np1.set_max_list_length(25)
 np1.set_max_table_rows(25 * 3, False)
 np1.poll()
