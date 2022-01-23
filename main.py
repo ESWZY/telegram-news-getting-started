40bbe7a571793f81efff12c3493962caf4286bbf
craziks-creator <67502395+craziks-creator@users.noreply.github.com>
2022-01-23 19:14:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -134,8 +134,8 @@ ie1 = InfoExtractor()
 # Select elements by CSS-based selector
 ie1.set_list_selector('div.inner_page > ul ') #id_ul_li
 ie1.set_title_selector('div.inner_page > ul > li ')  #id
-ie1.set_paragraph_selector('div.inner_page > ul > li  ')
-ie1.set_time_selector(' ')
+ie1.set_paragraph_selector('div.inner_page > ul > li ')
+ie1.set_time_selector('')
 ie1.set_source_selector('span.sourceTemplate')
 ie1.max_post_length = 2000
 ie1.set_id_policy(ssc_id_policy)
